e7d9db41ddfd9f2c01093907f74e49ca875beaa9
Michael Makalkin <regul8or@gmail.com>
2024-06-23 15:13:29 +0300

    Pool Metadata

--- vmback/__main__.py
+++ vmback/__main__.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import sys
+import pathlib
 
 from dotenv import dotenv_values
 
-
 from .config import Config
 from .misc import *
-
+from .pool import pool_backup
 
 def main(args):
-    print('Using', args.conf)
+    log(f'Using config file: {args.conf}')
 
     conf = get_conf(args.conf)
     if conf is None:
@@ -26,16 +27,32 @@ def main(args):
         password = env['XEN_PASSWORD']
 
     if username is None or password is None:
-        print('Missing username or password')
+        log('*** FATAL: Missing username or password')
         return -1
-    auth = { 'username': username, 'password': password }
-    conf.add('auth', auth)
+    conf.add('auth', { 'username': username, 'password': password })
+
+    prev_path = None
+    if 'backup_location' in conf['env']:
+        path = conf['env']['backup_location']
+        if pathlib.Path(path).exists():
+            prev_path = os.getcwd()
+            os.chdir(path)
+
+    log(f'Backup location: {os.getcwd()}')
+    if not os.access(os.getcwd(), os.W_OK):
+        log('*** FATAL: Location is not writable')
+        return -1
 
     if 'pools' in conf:
         pools = conf['pools']
-        print(pools)
         for pool in pools:
-            print(pool)
+            try:
+                pool_backup(pool, conf)
+            except Exception as err:
+                print(f'Unexpected {err=}, {type(err)=}')
+
+    if prev_path is not None:
+        os.chdir(prev_path)
 
     return 0
 
@@ -44,5 +61,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='vmback', description='Backup Xen VMs')
     parser.add_argument('-c', '--config', dest='conf', default='config.yaml', help='Specify a config .yaml file')
     args = parser.parse_args()
-    ret = main(args)
-    sys.exit(ret)
+    sys.exit(main(args))
--- vmback/misc.py
+++ vmback/misc.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+import subprocess
+from datetime import datetime
+
 from .config import Config
+
+
+complete_log = ''
 
 def get_conf(c):
     conf = None
@@ -10,3 +16,26 @@ def get_conf(c):
         print(e)
         conf = None
     return conf
+
+
+def log(msg):
+    global complete_log
+
+    now = datetime.now()
+    dt = now.strftime('%Y-%m-%d %H:%M:%S')
+    print(dt, msg)
+    complete_log += dt + ' ' + msg + '\n'
+
+
+def run_shell_command(cmd):
+    log(cmd)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    for line in process.stdout:
+        log(line.decode("utf-8"))
+    errcode = process.wait()
+    log(str(errcode))
+    return errcode
+
+
+def str_format(str, **kwargs):
+    return str.format(**kwargs)
